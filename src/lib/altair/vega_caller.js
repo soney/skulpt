72b4ebd46e0c43e27d0f25649b436435f9dd1c01
Bradley Miller <bradleymiller@google.com>
2019-06-05 07:12:11 -0500

    add semis

--- src/lib/altair/vega_caller.js
+++ src/lib/altair/vega_caller.js
@@ -13,9 +13,9 @@ var $builtinmodule = function (name) {
     mod.render_graph = new Sk.builtin.func(function (vegaspec) {
         Sk.builtin.pyCheckArgs("render_graph", arguments, 1, 1);
         // set actions to false to remove links to editor etc.
-        let divid = '#' + Sk.divid + '_graphics'
+        let divid = '#' + Sk.divid + '_graphics';
         try {
-            vegaEmbed(divid, Sk.ffi.remapToJs(vegaspec), {actions: true})
+            vegaEmbed(divid, Sk.ffi.remapToJs(vegaspec), {actions: true});
     } catch(e) {
         throw new Sk.builtin.Exception("Error in graph specification unable to render the graph\n" + e.message);
     }
