167dc4eff423492f57ee041066b8f52dc35e9221
Bradley Miller <bradleymiller@google.com>
2019-06-26 12:05:24 -0500

    Refactor to return chart objects, add __add__

--- src/lib/altair/__init__.py
+++ src/lib/altair/__init__.py
@@ -1,3 +1,4 @@
+import random
 try:
     from vega_caller import render_graph
 except:
@@ -6,44 +7,43 @@ except:
         print(js)
 
 class Chart:
-    # TODO allow data to be specified as a URL
+    # TODO allow data to be specified as a URL to a CSV or DB or json
     def __init__(self, data, title=None):
         self.title=title
+        self.data_id = random.randrange(1000000,5000000)
+        self.dsname = "data-{}".format(self.data_id)
         if isinstance(data, dict):
             self.data = Data(**data)
         else:
             self.data = data
 
-    def mark_bar(self):
-        self.mark = BarChart(self.data, title=self.title)
-        return self.mark
-
-    def mark_point(self):
-        self.mark = ScatterChart(self.data, title=self.title)
-        return self.mark
-
-    def mark_line(self):
-        self.mark = LineChart(self.data, title=self.title)
-        return self.mark
-
-class Mark:
-
-    def __init__(self, data, title=None):
-        self.data = data
-        self.title = title
         self.json = {}
-        self.json["$schema"] = "https://vega.github.io/schema/vega-lite/v2.json"
+        self.json["$schema"] = "https://vega.github.io/schema/vega-lite/v2.5.json"
         self.json['data'] = {}
         if self.title:
             self.json["title"] = self.title
 
-        # Now convert the data into a json format
-        self.json['data'] = {"values": data.vals}
+        self.json['data'] = {"values": self.data.vals}
+
+    def mark_bar(self):
+        self.mark = 'bar'
+        self.json['mark'] = 'bar'
+        return self
+
+    def mark_point(self):
+        self.mark = 'point'
+        self.json['mark'] = 'point'
+        return self
+
+    def mark_line(self):
+        self.mark = 'line'
+        self.json['mark'] = 'line'
+        return self
 
     def encode(self, x='', y='', color='', size=''):
         self.encoding = {}
         if x:
-            if isinstance(x,Axis): 
+            if isinstance(x,Axis):
                 self.encoding['x'] = x.toJson()
             else:
                 self.encoding['x'] = Axis(x).toJson()
@@ -62,7 +62,7 @@ class Mark:
         if size:
             field, tp = _get_name_type(size)
             self.encoding['size'] = dict(type=tp, field=field)
-            
+
         self.json['encoding'] = self.encoding
         return self
 
@@ -82,30 +82,42 @@ class Mark:
         self.json['selection'] = iconfig
         return self
 
+
     def display(self):
         render_graph(self.json)
 
-class BarChart(Mark):
-    def __init__(self, data, title=None):
-        # we will assume that the data is structured like you would make a dataframe.
-        # The keys are the name of the columns and the values are lists
+    def __add__(self, other):
+        self.json['layer'] = []
+        self.json['datasets'] = {}
+        temp = {}
+        temp['data'] = {"name": self.dsname }
+        self.json['datasets'][self.dsname] = self.json['data']['values']
+        del self.json['data']
+        temp['encoding'] = self.json['encoding']
+        del self.json['encoding']
+        temp['mark'] = self.json['mark']
+        del self.json['mark']
+        self.json['layer'].append(temp)
+        temp = {}
+        self.json['datasets'][other.dsname] = other.json['data']['values']
+        temp['data'] = {"name": other.dsname}
+        temp['encoding'] = other.json['encoding']
+        temp['mark'] = other.json['mark']
+        self.json['layer'].append(temp)
+        return self
 
-        Mark.__init__(self, data, title=title)        
-        self.json['mark'] = 'bar'
-
-class ScatterChart(Mark):
-
-    def __init__(self, data, title=None):
-        Mark.__init__(self, data, title=title)
-        self.json['mark'] = 'point'
-
-
-class LineChart(Mark):
-
-    def __init__(self, data, title=None):
-        Mark.__init__(self, data, title=title)
-        self.json['mark'] = 'line'
-
+#   "datasets": {
+#     "data-e56e43354d9b8f2ab0d1e90cf27cd47f": [
+#       {"a": 4, "b": 1, "c": "r"},
+#       {"a": 5, "b": 2, "c": "g"},
+#       {"a": 6, "b": 3, "c": "b"}
+#     ],
+#     "data-a4c3047a15bf9380c2e615cf87648369": [
+#       {"a": 1, "b": 4, "c": "r"},
+#       {"a": 2, "b": 5, "c": "g"},
+#       {"a": 3, "b": 6, "c": "b"}
+#     ]
+#   }
 
 # TODO:  add a rect mark type for a heat map
 
@@ -118,8 +130,6 @@ def _get_name_type(name):
     return nt[0], tstring
 
 class Axis:
-
-
 
     def __init__(self, name, bin=False):
         self.aggregate = None
@@ -152,8 +162,8 @@ class Y(Axis):
 class Data:
     def __init__(self, **kwargs):
         """
-        input can be the form of a series of keyword args where 
-        the keyword is the column name, or a single keyword data 
+        input can be the form of a series of keyword args where
+        the keyword is the column name, or a single keyword data
         that is a list of json style records as rows.
         could also accept a csv file? or keywords columns and rows
         This accepts a lot more options that "real altair" to make it easier
@@ -161,7 +171,7 @@ class Data:
         self.keys contains the list of column names
         self.vals is a list of dictionaries of the form [{col1:val1, col2:val2, ... coln:valn},
                                                          {col1:val3, col2:val4, ... coln:valn},
-                                                         {col1:val5, col2:val6, ... coln:valn}]                                                         
+                                                         {col1:val5, col2:val6, ... coln:valn}]
         """
         # todo: accept a URL as the argument to read a CSV or JSON file
         if len(kwargs) == 1 and 'data' in kwargs:
@@ -198,18 +208,20 @@ class Data:
         res = ""
         for row in self.vals:
             res += template.format(**row)
-        
+
         return header + res
-        
+
 
 
 
 if __name__ == '__main__':
     d = Data(a=[3,4,5], b=[1,2,3], c=['r','g','b'])
     print(d)
-    Chart({'a':list("abc"), 'b':[1,2,3]}).mark_bar().encode(x='a:N',y='b')
-    Chart(Data(a=[3,4,5], b=[1,2,3], c=['r','g','b'])).mark_point().encode(x='b',y='a',color='c:O')
-    Chart(Data(a=[1,2,3,2,2,4,5,5,6,7,8,8,8,8,8,9,0,0])).mark_bar().encode(Axis('a:Q', bin=True),y='count()')
+    print(type(Chart({'a':list("abc"), 'b':[1,2,3]}).mark_bar().encode(x='a:N',y='b')))
+    aa = Chart(Data(a=[3,4,5], b=[1,2,3], c=['r','g','b'])).mark_point().encode(x='b',y='a',color='c:O')
+    bb = Chart(Data(a=[1,2,3], b=[4,5,6], c=['r','g','b'])).mark_line().encode(x='b',y='a',color='c:O')
+    print((aa+bb).display())
+    #Chart(Data(a=[1,2,3,2,2,4,5,5,6,7,8,8,8,8,8,9,0,0])).mark_bar().encode(Axis('a:Q', bin=True),y='count()')
 
 
 # todo: see if I can work with the repr of an object to get the behavior that altair gets in notebooks
