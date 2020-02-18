1ff1e6a65912abd9cd8673bf49c1b00b96386ae7
Brad Miller <brad@runestoneinteractive.com>
2020-02-18 16:06:06 +0000

    add pythonds3

---
+++ src/lib/pythonds3/intro/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+pythonds3.intro import statement
+"""
+from pythonds3.intro.oop import Fraction
+
+__all__ = ["Fraction"]
---
+++ src/lib/pythonds3/searching/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+pythonds3.search import statement
+"""
+from pythonds3.searching.hash_table import HashTable
+
+__all__ = ["HashTable"]
